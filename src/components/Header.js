4fc8df009f78d26a33749584061e628b1d29457c
Training <training@Giulias-MacBook-Pro.local>
2018-07-02 16:53:31 +0100

    changed position

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = props => {
     <section className="header">
       <div className="wrapper">
         <h1 className="heading-1">Giulia G</h1>
-        <h2 className="heading-2">Front End <br /> Web Developer</h2>
+        <h2 className="heading-2">Scrum Master</h2>
         <a href="https://github.com/giuliagworld"><img className="img" src="/assets/github-circle.svg" width="50" /></a>
         <a href="https://linkedin.com/in/giuliagazzabin"><img className="img" src="/assets/linkedin-circle.svg" width="50" /></a>
       </div>
