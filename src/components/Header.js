b4df564d20cfdc022b1f2ee3524cdf04a4ed105d
Training <training@Giulias-MacBook-Pro.local>
2018-07-02 16:51:42 +0100

    update linkedin url

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = props => {
         <h1 className="heading-1">Giulia G</h1>
         <h2 className="heading-2">Front End <br /> Web Developer</h2>
         <a href="https://github.com/giuliagworld"><img className="img" src="/assets/github-circle.svg" width="50" /></a>
-        <a href="https://uk.linkedin.com/in/giuliagwebdeveloper"><img className="img" src="/assets/linkedin-circle.svg" width="50" /></a>
+        <a href="https://linkedin.com/in/giuliagazzabin"><img className="img" src="/assets/linkedin-circle.svg" width="50" /></a>
       </div>
     </section>
   )
